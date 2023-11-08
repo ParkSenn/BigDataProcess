1990be7557259f594c6c359488868e9d1c4f32f8
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2023-11-09 06:51:25 +0900

    Update IMDBStudent

--- HW2/IMDBStudent20210786.py
+++ HW2/IMDBStudent20210786.py
@@ -5,9 +5,10 @@ genre_count = dict()
 with open("movies_exp.txt", "rt") as fp:
     for line in fp:
             data = line.split("::")
-            genres = data[2].rstrip('\n')
+            genres = data[2]
             genre_list = genres.split("|") # genre를 구분한 리스트
             for genre in genre_list:
+                genre = genre.strip() # 양 옆 공백 다 제거
                 if genre not in genre_count:
                     genre_count[genre] = 1
                 else :
