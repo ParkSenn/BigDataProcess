d84c56c63b3d64eefc7961053d46ae368be5d9b0
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2023-11-10 06:37:32 +0900

    Feat: UBER

--- HW2/UBERStudent20210786.py
+++ HW2/UBERStudent20210786.py
@@ -4,7 +4,7 @@ import sys
 
 fileName = sys.argv[1]
 
-output = []
+output = dict()
 weekDay = "MON/TUE/WED/THU/FRI/SAT/SUN"
 weekDay_list = weekDay.split("/")
 
@@ -13,16 +13,24 @@ with open(fileName, "rt") as fp:
         info = line.split(",") # 지역, 날짜, 차량 수, 여행
         baseNum = info[0]
         date = info[1]
-        vehicles = info[2]
-        trips = info[3]
+        vehicles = int(info[2])
+        trips = int(info[3])       
         str_dt_list = date.split("/") 
         date_list = [int(i) for i in str_dt_list] # month, day, year
         day = calendar.weekday(date_list[2], date_list[0], date_list[1])
         wd = weekDay_list[day] # 구한 요일
-        result = f"{baseNum},{wd} {vehicles},{trips}"
-        output.append(result)
+
+        key = (baseNum, wd)
+        if key not in output :
+            output[key] = {"baseNum" : baseNum, "wd" : wd, 
+                           "vehicles" : vehicles, "trips" : trips}
+        else :
+            output[key]['vehicles'] += vehicles
+            output[key]['trips'] += trips
 
 
 outputName = sys.argv[2]
 with open(outputName, "w") as f:
-    f.writelines(output)
+    for key in output:
+        sample = output[key]
+        f.write("%s,%s %d,%d\n" % (sample["baseNum"], sample["wd"], sample["vehicles"], sample["trips"]))
